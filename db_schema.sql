05914b40301b80247207063d1b052b8d4af2bbc3
Alexey Prokopov <prokopov.alexey@gmail.com>
2023-05-20 12:02:07 +0300

    Intensity calculation

--- db_schema.sql
+++ db_schema.sql
@@ -1,42 +1,35 @@
 CREATE TABLE IF NOT EXISTS public.query
 (
-    id integer NOT NULL DEFAULT nextval('query_id_seq'::regclass),
-    query_string character varying COLLATE pg_catalog."default" NOT NULL,
+    id serial,
+    query_string character varying NOT NULL,
     start bigint NOT NULL,
     "end" bigint NOT NULL,
+    start_date date DEFAULT now(),
     CONSTRAINT query_pkey PRIMARY KEY (id)
 )
 
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.query
-    OWNER to scan;
-
--- Table: public.result
-
--- DROP TABLE IF EXISTS public.result;
 
 CREATE TABLE IF NOT EXISTS public.result
 (
-    id bigint NOT NULL DEFAULT nextval('result_id_seq'::regclass),
-    query_id bigint NOT NULL DEFAULT nextval('result_query_id_seq'::regclass),
-    url character varying COLLATE pg_catalog."default",
-    site character varying COLLATE pg_catalog."default",
-    title character varying COLLATE pg_catalog."default",
-    description text COLLATE pg_catalog."default",
-    content text COLLATE pg_catalog."default",
+    id bigserial NOT NULL,
+    query_id bigint NOT NULL,
+    url character varying,
+    site character varying,
+    title character varying,
+    description text,
+    content text,
     "number" bigint,
-    date_str character varying COLLATE pg_catalog."default",
-    encoding character varying COLLATE pg_catalog."default",
+    date_str character varying,
+    encoding character varying,
+    content_type character varying,
+    intensity_1 integer,
+    intensity_2 integer,
+    intensity_3 integer,
+    intensity_composite integer,
     intensity bigint,
     CONSTRAINT result_pkey PRIMARY KEY (id),
     CONSTRAINT query_id_fk FOREIGN KEY (query_id)
         REFERENCES public.query (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.result
-    OWNER to scan;
+);
--- scan.py
+++ scan.py
@@ -1,72 +1,251 @@
-import pycurl
 import urllib
 from htmldom import htmldom
 import requests
 import html
-import psycopg2
-
-class DB:
-    def __init__(self, url):
-        try:
-            self.conn = psycopg2.connect(url)
-        except:
-            print("Ошибка соединения с базой данных")
-        
-    def save_query(self, query_string, start, end):
-        with self.conn.cursor() as curs:
-            curs.execute('INSERT INTO "query" ("query_string", "start", "end") VALUES (%s, %s, %s) RETURNING id', (query_string, start, end))
-            query_id = curs.fetchone()[0]
-        self.conn.commit()
-        return query_id
-
-    def save_record(self, query_id, url, title, site, date_str, number, content):
-        with self.conn.cursor() as curs:
-            curs.execute('INSERT INTO "result" ("query_id", "url", "title", "site", "date_str", "number", "content") VALUES (%s, %s, %s, %s, %s, %s, %s)', (query_id, url, title, site, date_str, number, content))
-        self.conn.commit()
-    
+from peewee import *
+from datetime import *
+import re
 
 QUERY_STRING = "Приоритет 2030"
 START = 0
-END = 100
+END = 1000
 DB_URL = 'postgresql://scan@localhost:5432/scan'
 START_DATE='01.01.2017'
-END_DATE='15.05.2023'
+END_DATE=''
 
-db = DB(DB_URL);
+db = PostgresqlDatabase(DB_URL)
 
-query_id = db.save_query(QUERY_STRING, START, END)
-print("Запрос id=%s" % query_id)
+class Query(Model):
+    id = IntegerField(primary_key=True)
+    query_string = CharField()
+    start = IntegerField()
+    end = IntegerField()
+    start_date = DateField()
+    class Meta:
+        database=db
+        db_table='query'
     
-start = START
-number = START
-while start < END:
-    print ("Блок от %s\n" % start)
-    start = start + 10
-    tbs = urllib.parse.quote_plus('cdr:1,cd_min:' + START_DATE + ',cd_max:' + END_DATE)
-    query_url = 'https://www.google.com/search?q=' + urllib.parse.quote_plus(QUERY_STRING) + '&start=' + str(start) + '&tbs=' + tbs;
-    print('Query URL = ' + query_url)
-    resp = requests.get(query_url, headers={'Accept': 'text/html', 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0'})
-    dom = htmldom.HtmlDom().createDom(resp.content.decode(resp.encoding))
-    els=dom.find("div#rso div.yuRUbf")
-    for el in els:
-        href = el.find("a").attr("href")
-        print('url=' + href)
-        title = el.find("h3").text()
-        print('title=' + title)
-        date_str = el.parent().next().find('div')[0].find("span > span").text()
-        print('date=' + date_str)
-        site = el.find("span.VuuXrf").text()
-        print('site=' + site)
-        number = number + 1
-        if not href[0:4]=='http':
-            break
-        subResp = requests.get(href, allow_redirects=True, verify=False, headers={'Accept': 'text/html', 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0'})
-        print('encoding=' + subResp.encoding)
-        print('headers=' + subResp.headers)
-        try:
-            content = subResp.content.decode(subResp.encoding)
-        except:
-            print("Не удается декодировать содержимое")
-            content=None
-        db.save_record(query_id, href, title, site, date_str, number, content)    
+class Result(Model):
+    id = IntegerField(primary_key=True)
+    query_id = IntegerField()
+    url = CharField()
+    site = CharField()
+    title = CharField()
+    description = TextField()
+    content = TextField()
+    number = IntegerField()
+    date_str = TextField()
+    encoding = CharField()
+    content_type = CharField()
+    intensity = IntegerField()
+    intensity_1 = IntegerField()
+    intensity_2 = IntegerField()
+    intensity_3 = IntegerField()
+    intensity_composite = IntegerField()
+    class Meta:
+        database=db
+        db_table='result'
         
+def search(start, end):
+    query = Query.create(
+        query_string=QUERY_STRING, 
+        start=start, 
+        end=end
+    )
+    query.save()
+    print("Запрос id=%s" % query.id)
+    
+    start = START
+    number = START
+    while start < end:
+        print ("Блок от %s\n" % start)
+        start = start + 10
+        tbs = urllib.parse.quote_plus('cdr:1,cd_min:' + START_DATE + ',cd_max:' + END_DATE)
+        query_url = 'https://www.google.com/search?q=' + urllib.parse.quote_plus(QUERY_STRING) + '&start=' + str(start) + '&tbs=' + tbs + '&filter=0';
+        print('Query URL = ' + query_url)
+        resp = requests.get(query_url, headers={'Accept': 'text/html', 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0'})
+        dom = htmldom.HtmlDom().createDom(resp.content.decode(resp.encoding))
+        els=dom.find("div#search div.yuRUbf")
+        for el in els:
+            url = el.find("a").attr("href")
+            print('url=' + url)
+            title = el.find("h3").text()
+            print('title=' + title)
+            date_str = el.parent().next().find('div')[0].find("span > span").text()
+            print('date=' + date_str)
+            site = el.find("span.VuuXrf").text()
+            print('site=' + site)
+            number = number + 1
+            result = Result.create(
+                query_id = query.id,
+                url = url,
+                title = title,
+                site = site,
+                date_str = date_str,
+                number = number,
+            )
+            result.save()
+    return query
+        
+def apply(query_id, callback):
+    for result in Result.select().where(Result.query_id == query_id):
+        try:
+            callback(result)
+        except:
+            print('Ошибка в callback функции')
+
+
+def download(result):
+    print (result.id, result.url)
+    resp = requests.get(result.url, allow_redirects=True, verify=False, headers={'Accept': 'text/html', 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0'})
+    result.encoding = resp.encoding
+    result.content_type = resp.headers['Content-Type']
+    result.save()
+    try:
+        result.content = resp.content.decode(resp.encoding)
+        result.save()
+    except:
+        print("Не удается декодировать содержимое")
+    
+def calc_intensity_1(result):
+    value = 0
+    keywords = ['Приоритет\\s2030', 'Приоритет\\-2030']
+    print (result.id, result.url)
+    if (result.content != None):
+        for keyword in keywords:
+            r = re.findall(keyword, result.content);
+            value = value + len(r)
+    result.intensity_1 = value
+    result.save()
+
+def calc_intensity_3(result):
+    value = 0
+    keywords = ['Приоритет\\s2030', 'Приоритет\\-2030']
+    print (result.id, result.url)
+    if (result.content != None):
+        for keyword in keywords:
+            r = re.findall(keyword, result.title);
+            value = value + len(r)
+    result.intensity_3 = value
+    result.save()
+
+def calc_intensity_2(result):
+    value = 0
+    keywords = [
+        'программа', 
+        'Приоритет-2030', 
+        'ресурсы', 
+        'вклад', 
+        'университеты', 
+        'национальные цели', 
+        'развитие', 
+        'Российская Федерация', 
+        'научно-образовательный потенциал', 
+        'научные организации', 
+        'образовательные организации', 
+        'высшее образование', 
+        'социально-экономическое развитие', 
+        'цифровые технологии', 
+        'кадровое обеспечение', 
+        'инновации', 
+        'межинституциональное взаимодействие', 
+        'международное сотрудничество', 
+        'конкурс', 
+        'экспертиза', 
+        'грант', 
+        'российские университеты', 
+        'прогрессивные современные университеты', 
+        'научно-технологическое развитие', 
+        'страна', 
+        'программа развития', 
+        'университет', 
+        '2021-2030 годы', 
+        'реализация', 
+        'стратегическое академическое лидерство', 
+        'Министерство науки и высшего образования', 
+        'отбор', 
+        'участие', 
+        'цифровые кафедры', 
+        'цифровая кафедра', 
+        'программа', 
+        'Приоритет 2030', 
+        'цель', 
+        'университеты', 
+        'лидеры', 
+        'научное знание', 
+        'технологии', 
+        'разработки', 
+        'российская экономика', 
+        'социальная сфера', 
+        'высшее образование', 
+        'практики', 
+        'научно-исследовательская деятельность', 
+        'инновации', 
+        'образовательная деятельность', 
+        'привлекательность', 
+        'регионы России', 
+        'иностранные студенты', 
+        'зарубежные ученые', 
+        'навыки', 
+        'умения', 
+        'рынок труда', 
+        'научно-технологический прогресс', 
+        'государственная поддержка', 
+        'конкурентоспособность', 
+        'трансформация', 
+        'мировой рынок', 
+        'интеграция', 
+        'личностный потенциал', 
+        'качество жизни', 
+        'самореализация', 
+        'задачи', 
+        'исследования', 
+        'разработки', 
+        'кадровое обеспечение', 
+        'инновационный потенциал', 
+        'экономика страны', 
+        'субъекты Российской Федерации', 
+        'научно-технологический потенциал', 
+        'продукты', 
+        'сетевое взаимодействие', 
+        'международное сотрудничество', 
+        'цифровые компетенции', 
+        'ИТ-специальности', 
+        'образовательные услуги', 
+        'научно-технические услуги', 
+        'социальные услуги'
+    ]
+    print (result.id, result.url)
+    if (result.content != None):
+        for keyword in keywords:
+            r = re.findall(keyword, result.content);
+            value = value + len(r)
+    result.intensity_2 = value
+    result.save()
+            
+def calc_intensity_composite(result):
+    result.intensity_composite = result.intensity_1*10 + result.intensity_2+result.intensity_3*100
+    result.save()
+
+def calc_intensity(result):
+    THREASHOLD_1=100
+    THREASHOLD_2=300
+    THREASHOLD_3=500
+    if result.intensity_composite > THREASHOLD_3:
+        result.intensity = 3
+    elif result.intensity_composite > THREASHOLD_2:
+        result.intensity = 2
+    elif result.intensity_composite > THREASHOLD_1:
+        result.intensity = 1
+    else:
+        result.intensity = 0
+    result.save()
+
+query = search(0, 1000)    
+query_id = query.id
+apply(query_id, download)
+apply(query_id, calc_intensity_1)
+apply(query_id, calc_intensity_2)
+apply(query_id, calc_intensity_3)
+apply(query_id, calc_intensity_composite)
+apply(query_id, calc_intensity)
